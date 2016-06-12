6f52c99da30f271b965003238b7084dd5a1d89bb
ojlee <dldhwn56@gmail.com>
2016-06-13 03:25:24 +0900

    add windmill meta

---
+++ Assets/Ground.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ground : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //float zRotation = transform.localEulerAngles.z;
+        //zRotation = zRotation - Input.GetAxis("Horizontal");
+        //transform.localEulerAngles = new Vector3(10, 0, zRotation);
+        //if (Input.touchCount > 0 || Input.GetMouseButton(0))
+        {
+            //Debug.Log("mouse down:"+Input.mousePosition);
+            //if (Input.mousePosition.x < Screen.width / 2)
+            {//왼쪽을 클릭
+                //transform.localEulerAngles = new Vector3(10, 0, transform.localEulerAngles.z+1);
+
+            }
+            //else
+            {//오른쪽을 클릭
+                //transform.localEulerAngles = new Vector3(10, 0, transform.localEulerAngles.z - 1);
+            }
+        }
+
+	}
+}
---
+++ Assets/MH_Coin.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MH_Coin : MonoBehaviour {
+    void OnTriggerEnter(Collider col)
+    {
+        if(col.gameObject.name == "AtomBall")
+        {
+            GameObject.Find("GameManager").SendMessage("GetCoin");
+            Destroy(gameObject);
+        }
+    }
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(new Vector3(0.0f, 0.0f, 2.5f));
+	
+	}
+}
