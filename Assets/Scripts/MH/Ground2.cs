d5f70311e5e2fb9ad18b36cd9b8cbba8f5a26901
Lming1 <spear131121@gmail.com>
2016-06-15 04:25:17 +0900

    Hidden Stage 추가 및 카메라 변경사항 적용

---
+++ Assets/Scripts/MH/Ground2.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ground2 : MonoBehaviour {
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.name == "AtomBall")
+        {
+
+            GameObject.Find("GameManager").SendMessage("HiddenStageStart");
+            Destroy(gameObject);
+        }
+    }
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
