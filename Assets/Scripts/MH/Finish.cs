098732f8fd425f2607f0958b2d85e18663083d8d
Lming1 <spear131121@gmail.com>
2016-06-15 20:51:41 +0900

    finish point, jump sound add

---
+++ Assets/Scripts/MH/Finish.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+namespace MH
+{
+    public class Finish : MonoBehaviour
+    {
+        void OnCollisionEnter(Collision collision)
+        {
+            if (collision.gameObject.name == "AtomBall")
+            {
+
+                GameManager gmComponent = GameObject.Find("GameManager").GetComponent<GameManager>();
+                gmComponent.FinishPoint();
+            }
+        }
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
