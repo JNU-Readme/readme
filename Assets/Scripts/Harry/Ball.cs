f5a75daff34ebde4f7b661690f6b098d5ded67b2
JinHyeok Kang <jinhyeok.kang@gmail.com>
2016-06-17 01:34:29 +0900

    공 360도 적용.

--- Assets/Scripts/Harry/Ball.cs
+++ Assets/Scripts/Harry/Ball.cs
@@ -6,38 +6,102 @@ namespace Harry
     public class Ball : MonoBehaviour
     {
         float startingPoint;
-        //bool ballSpeed;
-        //bool isforward;
-        // Use this for initialization
+        public float jumpForce = 350f;
+        private bool grounded = false;
+        private bool jump;
+        Rigidbody rigdbody;
+        public float speed = 6f;
 
-
-        void Start()
+        private Vector3 prevPosition;
+        private Vector3 prevDiff;
+        
+        void Awake()
         {
-
-            startingPoint = transform.position.z;
+            rigdbody = GetComponent<Rigidbody>();
         }
 
         // Update is called once per frame
         void Update()
         {
-            //float distance;
-            //distance = transform.position.z - startingPoint;
-            if (Input.GetKeyDown(KeyCode.Space))
+            Vector3 diff = transform.position - prevPosition;
+            if (diff.x != 0 || diff.z != 0 || prevDiff == null)
             {
-                GetComponent<Rigidbody>().AddForce(Vector3.up * 300);
+                prevDiff = diff;
             }
+
+            float angleY = Mathf.Atan2(diff.x, diff.z) * 180 / Mathf.PI;
+            float z = 0;
+            float x = 0;
+
+            CheckGround();
+            if (Input.GetButtonDown("Jump") && grounded)
+                jump = true;
             if (Input.GetKey(KeyCode.LeftArrow))
             {
-                GetComponent<Rigidbody>().AddForce(Vector3.left * 5);
+                if (diff.x == 0 && diff.z == 0 && prevDiff != null)
+                {
+                    diff = prevDiff;
+                }
+
+                z = (Mathf.Cos((angleY - 90) * Mathf.PI / 180)) / 2;
+                x = (Mathf.Sin((angleY - 90) * Mathf.PI / 180)) / 2;
             }
             if (Input.GetKey(KeyCode.RightArrow))
             {
-                GetComponent<Rigidbody>().AddForce(Vector3.right * 5);
+                if (diff.x == 0 && diff.z == 0 && prevDiff != null)
+                {
+                    diff = prevDiff;
+                }
+                
+                z = (Mathf.Cos((angleY + 90) * Mathf.PI / 180)) / 2;
+                x = (Mathf.Sin((angleY + 90) * Mathf.PI / 180)) / 2;
             }
+
             if (Input.GetKey(KeyCode.UpArrow))
             {
-                GetComponent<Rigidbody>().AddForce(Vector3.forward * 5);
+                z = +(Mathf.Cos(angleY * Mathf.PI / 180));
+                x = +(Mathf.Sin(angleY * Mathf.PI / 180));
+                //GetComponent<Rigidbody>().AddForce(diff * speed);
             }
+            if (Input.GetKey(KeyCode.DownArrow)) //감속
+            {
+                z = -(Mathf.Cos(angleY * Mathf.PI / 180));
+                x = -(Mathf.Sin(angleY * Mathf.PI / 180));
+                //GetComponent<Rigidbody>().AddForce(diff * -1);
+            }
+
+            GetComponent<Rigidbody>().AddForce((new Vector3(x, 0, z) * speed) + diff);
+
+            prevPosition = transform.position;
+
+        }
+        void FixedUpdate()
+        {
+            if (jump)
+            {
+                rigdbody.AddForce(new Vector3(0f, jumpForce, 0f));
+                //rigdbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+                jump = false;
+                GetComponent<AudioSource>().Play();
+            }
+
+
+        }
+
+
+
+        void CheckGround()
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, Vector3.down, out hit, 0.9f))
+            {
+                if (hit.transform.tag == "GROUND")
+                {
+                    grounded = true;
+                    return;
+                }
+            }
+            grounded = false;
         }
 
     }
